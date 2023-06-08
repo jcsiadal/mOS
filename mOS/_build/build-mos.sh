64bc654616f8c6ba6da364d6bb4ebd25b82091ee
Balazs Gerofi <balazs.gerofi@intel.com>
2023-06-08 16:47:02 +0000

    Fix SUSE build script sed issue and baseline commit hash

--- mOS/_build/build-mos.sh
+++ mOS/_build/build-mos.sh
@@ -21,11 +21,29 @@ source ${RUNDIR}/mOS.conf
 # Check the kernel-source
 cd ${KSRCDIR}
 if ! [ "$(git rev-parse --short HEAD)" = "${KS_HEAD}" ]; then
-    echo "Current kernel-source HEAD is incorrect, it must be ${KS_HEAD}"
-    exit 1
-elif ! [ -f linux-${KVER}.tar.xz ] || ! [ -f linux-${KVER}.tar.sign ]; then
-    echo "Missing (.xz) source or signature for kernel ${KVER}"
-    exit 1
+    echo "Checking out kernel-source HEAD: ${KS_HEAD}"
+    if ! git reset --hard; then
+        echo "error: resetting repository for checking out ${KS_HEAD}"
+        exit 1
+    fi
+    if ! git checkout ${KS_HEAD}; then
+        echo "error: checking out ${KS_HEAD}"
+        exit 1
+    fi
+fi
+
+if ! [ -f linux-${KVER}.tar.xz ] || ! [ -f linux-${KVER}.tar.sign ]; then
+    KVER_X="v`echo ${KVER} | cut -d"." -f1`.x"
+	echo "Downloading linux-${KVER}.tar.xz..."
+    if ! wget https://cdn.kernel.org/pub/linux/kernel/${KVER_X}/linux-${KVER}.tar.xz 2>/dev/null; then
+        echo "error: downloading (.xz) source for kernel ${KVER}"
+        exit 1
+    fi
+	echo "Downloading linux-${KVER}.tar.sign..."
+    if ! wget https://cdn.kernel.org/pub/linux/kernel/${KVER_X}/linux-${KVER}.tar.sign 2>/dev/null; then
+        echo "error: downloading signature for kernel ${KVER}"
+        exit 1
+    fi
 fi
 
 # Check the mOS source
@@ -44,12 +62,8 @@ cat config.mos > $KSRCDIR/config/x86_64/mOS
 cd $KSRCDIR
 
 # Update the build configuration
-if ! [ -f config.backup ]; then
-    cp config{.conf,.backup}
-fi
-if [ -f config.backup ]; then
-    printf "+x86_64\tx86_64/mOS\n" > config.conf
-fi
+git checkout config.conf
+printf "+x86_64\tx86_64/mOS\n" > config.conf
 
 # Add the mOS patch to the list of kernel patches
 if ! $(grep -q "/mOS.patch" series.conf); then
@@ -62,23 +76,12 @@ if ! $(grep -q "=== kernel-mOS ===" rpm/package-descriptions); then
 fi
 
 # Update the specfile templates
-if ! [ -f rpm/kernel-binary.spec.backup ]; then
-    cp rpm/kernel-binary.spec.{in,backup}
-fi
-if [ -f rpm/kernel-binary.spec.backup ]; then
-    rm -f rpm/kernel-binary.spec.in
-    cp rpm/kernel-binary.spec.{backup,in}
-    sed -ni "/^# The following is copied to the -base subpackage/e cat ${RUNDIR}/spec-requires" rpm/kernel-binary.spec.in
-    sed -ni "/^%changelog/e cat ${RUNDIR}/spec-package" rpm/kernel-binary.spec.in
-fi
-if ! [ -f rpm/kernel-syms.spec.backup ]; then
-    cp rpm/kernel-syms.spec.{in,backup}
-fi
-if [ -f rpm/kernel-syms.spec.backup ]; then
-    rm -f rpm/kernel-syms.spec.in
-    cp rpm/kernel-syms.spec.{backup,in}
-    sed -ni "/^Release:.*%kernel_source_release/d" rpm/kernel-syms.spec.in
-fi
+git checkout rpm/kernel-binary.spec.in
+sed -i "/^# The following is copied to the -base subpackage/e cat ${RUNDIR}/spec-requires" rpm/kernel-binary.spec.in
+sed -i "/^%changelog/e cat ${RUNDIR}/spec-package" rpm/kernel-binary.spec.in
+
+git checkout rpm/kernel-syms.spec.in
+sed -i "/^Release:.*%kernel_source_release/d" rpm/kernel-syms.spec.in
 
 # Generate the new kernel source tarball
 ./scripts/tar-up.sh -a x86_64 -f mOS -rs "$RELEASE+mOS_$MOS_HEAD"
